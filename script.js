175ec188de25f80da026562b7ff8a68e9471a1ce
Lucca Kruger <91633728+krugerlucca@users.noreply.github.com>
2023-03-10 21:32:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-document.getElementsByClassName("cabeca").onmouseover = function () {
-  mouseOver();
-};
-document.getElementsByClassName("cabeca").onmouseout = function () {
-  mouseOut();
-};
-
-function mouseOver() {
-  console.log("entrou");
+function calcularIdade(dataNascimento) {
+  var hoje = new Date();
+  var nascimento = new Date(dataNascimento);
+  var idade = hoje.getFullYear() - nascimento.getFullYear();
+  var mes = hoje.getMonth() - nascimento.getMonth();
+  if (mes < 0 || (mes === 0 && hoje.getDate() < nascimento.getDate())) {
+    idade--;
+  }
+  return idade;
 }
+var idade = calcularIdade('2005-07-31');
 
-function mouseOut() {
-  console.log("saiu");
+function txt_idade(){
+	document.getElementById("txt_idade").innerHTML =idade+" anos";
 }
